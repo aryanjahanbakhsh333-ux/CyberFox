c6d544b1c7eefa784b0079533caa5ab3051cdbe6
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-11 06:29:15 +0330

    Create security_headers.py

---
+++ security_headers.py
@@ -0,0 +1,33 @@
+def apply_security_headers(response):
+    response.headers[
+        "X-Content-Type-Options"
+    ] = "nosniff"
+
+    response.headers[
+        "X-Frame-Options"
+    ] = "DENY"
+
+    response.headers[
+        "Referrer-Policy"
+    ] = "strict-origin-when-cross-origin"
+
+    response.headers[
+        "Permissions-Policy"
+    ] = (
+        "camera=(), microphone=(), "
+        "geolocation=()"
+    )
+
+    response.headers[
+        "Content-Security-Policy"
+    ] = (
+        "default-src 'self'; "
+        "script-src 'self' 'unsafe-inline'; "
+        "style-src 'self' 'unsafe-inline'; "
+        "img-src 'self' data:; "
+        "connect-src 'self'; "
+        "object-src 'none'; "
+        "frame-ancestors 'none';"
+    )
+
+    return response

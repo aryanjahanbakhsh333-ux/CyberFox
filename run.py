af4b68a05d153e01f0e1d3a37d61fa8e79dfeb68
aryanjahanbakhsh333-ux <aryan.jahanbakhsh333@gmail.com>
2026-09-11 05:33:18 +0330

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+
+from app_factory import create_app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    port = int(
+        os.getenv(
+            "PORT",
+            "5000"
+        )
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
